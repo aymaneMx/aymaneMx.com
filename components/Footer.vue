f6a30e5188c28534757e1ff4c48a5b414a8b1463
aymaneMx <aymanemx@gmail.com>
2021-11-21 02:59:22 +0100

    :foot: update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,11 +3,10 @@
     <hr class="h-px mt-6 border-gray-300 max-w-screen-xl mx-auto">
     <div class="md:py-5">
       <div class="flex flex-col items-center justify-between mt-6 md:mt-0 md:flex-row">
-        <div class="logo">
-          <nuxt-link to="/">
-            <h1 class="text-xl font-semibold text-gray-700 dark:text-gray-200"> <span
-              class="text-primary font-bold">Aymane</span>Mx.</h1>
-          </nuxt-link>
+        <div class="logo flex items-center">
+          <p class="text-sm text-gray-700 dark:text-gray-200 mx-1">© 2021 aymaneMx Built with ♡ </p>
+          <a aria-label="AymaneMx.com" class="text-sm text-gray-700 dark:text-gray-200"
+             href="https://github.com/aymaneMx/aymaneMx.com">Open sourced on Github</a>
         </div>
 
 
